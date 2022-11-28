482cf683d1a9e82d30d2286c1f6e34ff92fdc0b8
bryaanls <bryanluccas@hotmail.com>
2022-11-28 13:39:13 -0300

    Página do cadastro

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="shortcut icon" href="/imagens/favicon.ico" type="image/x-icon">
+    <title>Cadastro</title>
+</head>
+
+<body>
+    <header>
+        <h1>Faça um cadastro</h1>
+    </header>
+
+    <main>
+        <div class="container">
+            <h1>Cadastre-se! ;) </h1>
+            <form action="dados_cad.php" method="post" id="cadastro">
+                <label for="nome">Nome</label>
+                <input type="text" name="nome" placeholder="*OBRIGATÓRIO" required>
+
+                <label for="email">E-mail</label>
+                <input type="email" name="email" placeholder="*OBRIGATÓRIO" required>
+
+                <label for="sexo">Sexo</label>
+                <select name="sexo" required>
+                    <option value="">Selecione seu gênero</option>
+                    <option value="Masculino">Masculino</option>
+                    <option value="Feminino">Feminino</option>
+                </select>
+
+                <label for="Apelido">Apelido</label>
+                <input type="apelido" name="apelido" placeholder="*Opcional">
+            </form>
+            <button type="submit" form="cadastro" id="btn_cad">Cadastrar</button>
+        </div>
+    </main>
+    <footer>
+        <ul class="copy">
+            <li>Bryan Soares</li>
+            <li>&copy;Todos os direitos reservados</li>
+        </ul>
+        <ul class="links">
+            <li><a href="#">Instagram</a> </li>
+            <li><a href="#">Linkedin</a> </li>
+            <li><a href="#">GitHub</a> </li>
+        </ul>
+    </footer>
+
+</body>
+
+</html>
